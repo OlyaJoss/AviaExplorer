d13eeb91713c27d4ff0d9d8735189627f9c8ace9
OlyaJoss <65188310+OlyaJoss@users.noreply.github.com>
2022-10-24 11:47:48 +0300

    created left input button

--- src/Form.js
+++ src/Form.js
@@ -22,35 +22,35 @@ function Form(props) {
     const [autoHints, setAutoHints] = useState([]);
     const [isFormValid, setIsFormValid] = useState(false);
     const [isFromAirportValid, setIsFromAirportValid] = useState(false);
+    const [isToAirportValid, setIsToAirportValid] = useState(false);
 
-    const getData = () => {
-        // json => js
-        fetch(`http://autocomplete.travelpayouts.com/places2?term=${fromAirport}&locale=en&types[]=city,airport,country`)
+    const getData = (text) => {
+        fetch(`http://autocomplete.travelpayouts.com/places2?term=${text}&locale=en&types[]=city,airport,country`)
             .then(result => result.json())
             .then(data => setAutoHints(data))
             .catch(err => console.error(err));
     }
-    
+
     return (
         <>
-        
+
             <Flex as='form'
-            onSubmit={(e) => {
-                isFormValid 
-                ? props.onSearch(e, fromAirport, toAirport, startDate.toISOString().split('T')[0].slice(0, 7))
-                : e.preventDefault()
-            }}
+                onSubmit={(e) => {
+                    isFormValid
+                        ? props.onSearch(e, fromAirport, toAirport, startDate.toISOString().split('T')[0].slice(0, 7))
+                        : e.preventDefault()
+                }}
                 alignItems='flex-end' justify='space-between' flexDirection='row' pb='10px' w='986px'>
                 <FormControl w='282px'>
                     <FormLabel color='#474A51'>
                         From
                     </FormLabel>
-                
+                    {/* ПРАВЫЙ ИНПУТ */}
                     <Input
                         value={fromAirport}
                         onChange={(event) => {
                             setFromAirport(event.target.value)
-                            getData()
+                            getData(fromAirport)
                             setIsFormValid(false)
                             setIsFromAirportValid(false)
                         }}
@@ -61,8 +61,8 @@ function Form(props) {
                         color='#1F2229'
                         fontWeight='700'
                         borderColor='#D8D8D8' />
-                      
-                   
+
+
                 </FormControl>
 
                 <IconButton aria-label='Replace cities'
@@ -77,9 +77,14 @@ function Form(props) {
                     <FormLabel color='#474A51'>
                         To
                     </FormLabel>
+                    {/* ЛЕВЫЙ ИНПУТ */}
                     <Input
                         value={toAirport}
-                        onChange={(event) => setToAirport(event.target.value)}
+                        onChange={(event) => {
+                            setToAirport(event.target.value)
+                            getData(toAirport)
+                            setIsToAirportValid(false)
+                        }}
                         placeholder='Airport, City or Country'
                         _placeholder={{ fontWeight: 500 }}
                         h='64px' w='282px'
@@ -106,21 +111,39 @@ function Form(props) {
             </Flex>
             {/* TODO: лимит  на подсказки, показывать первые 5 */}
             <Box pb='80px' w='350px'>
-            {
-                           isFromAirportValid 
-                           ? null
-                           : <Text>Choose variant from below</Text>
-                       } 
-            {autoHints.slice(0, 5).map(({ name, code, country_name, id }) => (
-                        <Button key={id} color='#FFFFFF' bgColor='#7B61FF' size='xs' mr='3px' 
+                {
+                    isFromAirportValid
+                        ? null
+                        : <Text>Choose variant from below</Text>
+                }
+                {autoHints.slice(0, 5).map(({ name, code, country_name, id }) => (
+                    <Button key={id} color='#FFFFFF' bgColor='#7B61FF' size='xs' mr='3px'
                         onClick={() => {
-                        setFromAirport(name)
-                        setIsFormValid(true)
-                        setIsFromAirportValid(true)
+                            setFromAirport(name)
+                            setIsFormValid(true)
+                            setIsFromAirportValid(true)
                         }}>
-                            {name} {country_name} {code}
-                        </Button>)
-                    )}
+                        {name} {country_name} {code}
+                    </Button>)
+                )}
+            </Box>
+
+            <Box pb='80px' w='350px'>
+                {
+                    isToAirportValid
+                        ? null
+                        : <Text>Choose variant from below</Text>
+                }
+                {autoHints.slice(0, 5).map(({ name, code, country_name, id }) => (
+                    <Button key={id} color='#FFFFFF' bgColor='#7B61FF' size='xs' mr='3px'
+                        onClick={() => {
+                            setToAirport(name)
+                            setIsFormValid(true)
+                            setIsToAirportValid(true)
+                        }}>
+                        {name} {country_name} {code}
+                    </Button>)
+                )}
             </Box>
         </>
     )
